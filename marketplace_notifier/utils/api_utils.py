10e50362022e21dda15091920fe8b0f2a5e84ccb
Goddeeris Edouard <edouard.goddeeris@student.hogent.be>
2023-09-20 01:24:24 +0200

    Added postal code / city API

---
+++ marketplace_notifier/utils/api_utils.py
@@ -0,0 +1,33 @@
+import logging
+from http import HTTPStatus
+from typing import Optional, Dict
+
+import aiohttp
+
+logging.basicConfig(level=logging.INFO, filename="requests.log",
+                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+
+async def get_request_response(client_session: aiohttp.ClientSession, URI: str,
+                               headers: Optional[Dict] = None) -> str:
+    """
+    uses client_session with given headers and a user-agent
+    logs errors
+    """
+    if headers is None:
+        headers = {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"}
+    elif "user-agent" not in headers:
+        headers = dict(headers, **{
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
+
+    logging.info("making request for %s", URI)
+    async with client_session.get(URI, headers=headers, ssl=False) as response:
+        if response.status == HTTPStatus.OK:
+            return await response.text()
+    logging.warning("Got status code %d on %s, trying again once more", response.status, URI)
+
+    async with client_session.get(URI, headers=headers, ssl=False) as response:
+        if response.status == HTTPStatus.OK:
+            return await response.text()
+    logging.error("Failed again, got error %d\n%s", response.status, response)
---
+++ tests/test_postalcode_api.py
@@ -0,0 +1,45 @@
+import asyncio
+import unittest
+
+import aiohttp
+
+from marketplace_notifier.notifier.tweedehands.api_models import LocationFilter
+
+# https://www.reddit.com/r/learnpython/comments/11q8i08/comment/jc7fb6a/
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+
+class TestPostalCodeAPI(unittest.IsolatedAsyncioTestCase):
+    def test_invalid_postal_code_should_raise_value_error(self):
+        postal_code = 1
+
+        with self.assertRaises(ValueError):
+            _ = LocationFilter.belgian_postal_code_validation(postal_code)
+
+    async def test_valid_postal_code_should_return_correct_city(self):
+        given_postal_code = 9000
+        expected_city = "Gent"
+
+        async with aiohttp.ClientSession() as client:
+            result = await LocationFilter.get_valid_postal_code_and_city(client_session=client,
+                                                                         postal_code_or_city=str(given_postal_code))
+        self.assertEqual(result["city"], expected_city)
+        self.assertEqual(result["postal_code"], given_postal_code)
+
+    async def test_valid_city_should_have_expected_postal_code_in_result(self):
+        given_city = "Brussel"
+        expected_postal_code = 1000
+
+        async with aiohttp.ClientSession() as client:
+            result = await LocationFilter.get_valid_postal_code_and_city(client_session=client,
+                                                                         postal_code_or_city=given_city)
+        self.assertEqual(result["postal_code"], expected_postal_code)
+        self.assertEqual(result["city"], given_city)
+
+    async def test_invalid_city_should_return_none(self):
+        given_city = "amsterdam"
+
+        async with aiohttp.ClientSession() as client:
+            result = await LocationFilter.get_valid_postal_code_and_city(client_session=client,
+                                                                         postal_code_or_city=given_city)
+        self.assertIsNone(result)
