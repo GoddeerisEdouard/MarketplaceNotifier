a596b6a46433faedaa5794cafa1921cf5937b4cb
Goddeeris Edouard <edouard.goddeeris@student.hogent.be>
2023-11-20 19:21:29 +0100

    Refactored notifier interface

--- marketplace_notifier/notifier/models.py
+++ marketplace_notifier/notifier/models.py
@@ -1,5 +1,6 @@
+from __future__ import annotations
 from dataclasses import dataclass
-from abc import ABC
+from abc import ABC, abstractmethod
 from datetime import datetime
 from enum import Enum
 from typing import Optional
@@ -7,7 +8,7 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 
-# NOTE: these classes are used as returnmodels when using functions
+# NOTE: these classes are used as input / output models when using functions
 
 
 class IPriceType(BaseModel, ABC, Enum):
@@ -32,7 +33,7 @@ class Location(BaseModel):
 
 
 @dataclass
-class IListingInfo(BaseModel, ABC):
+class IListingInfo(ABC):
     """
     interface represents all relevant listing info
     """
@@ -47,5 +48,48 @@ class IListingInfo(BaseModel, ABC):
     specified_location: Location
     vip_url: str
 
-    def get_link(self) -> str:
+    def get_full_url(self) -> str:
+        """
+        :return: full URL hyperlink to listing
+        """
         return self._BASE_URL + self.vip_url
+
+
+@dataclass
+class PriceRange:
+    min_price_cents: int
+    max_price_cents: int
+
+
+@dataclass
+class IListingSpecs(ABC):
+    """
+    listing specifications given by user
+    these will be parsed into a URL to make a GET REQUEST
+    """
+    query: str
+    location: Location
+    price_range: PriceRange = PriceRange(0, 0)
+
+    @abstractmethod
+    def generate_listing_query_url(self) -> str:
+        """
+        generates request URL based on the given attributes
+        ( does the opposite of parse_url(...) )
+        :return: a listing query url to use for a GET request
+        """
+        raise NotImplementedError()
+
+    @classmethod
+    @abstractmethod
+    def parse_url(cls, get_request_query_url: str) -> IListingSpecs:
+        """
+        parses url and returns object of this dataclass
+        ( does the opposite of get_request_url(...) )
+        :param get_request_query_url: the url to parse
+        looks like .../q/iphone/#Language:all-languages|sortBy:SORT_INDEX|sortOrder:DECREASING|postcode:9000|searchInTitleAndDescription:true
+        or
+        .../q/iphone/#Language:all-languages|PriceCentsTo:5000|sortBy:SORT_INDEX|sortOrder:DECREASING|postcode:9000|searchInTitleAndDescription:true
+        :return: this dataclass, parsed by current url
+        """
+        raise NotImplementedError()
--- marketplace_notifier/notifier/notifier.py
+++ marketplace_notifier/notifier/notifier.py
@@ -1,12 +1,57 @@
 from abc import ABC, abstractmethod
+from typing import List, Optional
 
-from marketplace_notifier.notifier.models import IListingInfo
+import aiohttp
+
+from marketplace_notifier.notifier.models import IListingInfo, IListingSpecs
 
 
 class INotifier(ABC):
     """
     interface of marketplace notifier
     """
+    # TODO: add unique ID to every element in this list to be able to easily remove them
+    # (maybe based on name or something, just a generated name, can be the vip url too)
+    # the request url is not enough, as it looks different for the developer than for the user
+    # these urls are to fetch ALL listings of a specific spec
+    # example:
+    # browser URL req: .../q/nintendo+switch
+    # GET req URL: .../lrp/api/search?attributesByKey[]=Language%3Aall-languages&limit=30&offset=0&query=nintendo%20switch&searchInTitleAndDescription=true&viewOptions=list-view
+    listing_urls_for_requests: List[str] = []
+
     @abstractmethod
-    async def get_listing(self) -> IListingInfo:
-        pass
+    async def _parse_listings_from_response(self, client_session: aiohttp.ClientSession, get_req_url: str) -> List[
+        Optional[IListingInfo]]:
+        """
+        helper method
+        gets a raw json response with all listings of query & returns IListingInfo objects
+        :param client_session: session to use the send GET requests
+        :param get_req_url: exact GET url to request all raw listings json
+        :return: a (empty) list of IListingInfo objects
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def add_new_query(self, listing_specs: IListingSpecs) -> None:
+        """
+        adds parsed "listing query"/~listing_specs to notifier lising query list
+        :return: None
+        """
+
+        request_url = listing_specs.generate_listing_query_url()
+
+        # append to query request list to refresh every now & then
+        self.listing_urls_for_requests.append(request_url)
+
+    @abstractmethod
+    async def get_listings_of_request_url(self, client_session: aiohttp.ClientSession, query_request_url: str) -> List[
+        IListingInfo]:
+        """
+        gets all listings and returns all parsed relevant listing info in a list
+        :param client_session: session to use the send GET requests
+        :param query_request_url: exact GET request url used to get all listings
+        :return: parsed listing info as dataclass object
+        """
+
+        new_listings = await self._parse_listings_from_response(client_session, query_request_url)
+        return new_listings
