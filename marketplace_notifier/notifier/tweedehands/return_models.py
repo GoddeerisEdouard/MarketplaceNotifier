cae89068fec8f472c3de66be8f07fba4f5b328da
Goddeeris Edouard <edouard.goddeeris@student.hogent.be>
2023-11-21 21:19:53 +0100

    Updated tweedehands return models

--- marketplace_notifier/notifier/tweedehands/return_models.py
+++ marketplace_notifier/notifier/tweedehands/return_models.py
@@ -1,9 +1,12 @@
-from pydantic import field_validator, Field
+from pydantic import Field
 
 from marketplace_notifier.notifier.models import IListingInfo
 
 
 class TweedehandsListing(IListingInfo):
-    _BASE_URL = "https://www.2dehands.be"
     id: str = Field(max_length=11)
     title: str = Field(max_length=60)
+
+    @property
+    def BASE_URL(self) -> str:
+        return "https://www.2dehands.be"
